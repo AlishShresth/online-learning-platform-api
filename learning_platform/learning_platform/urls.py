2453c1d9dd17cee5263a185f820dc3c7af33eed7
Alish Shrestha <cresthaalish@gmail.com>
2025-05-30 10:49:00 +0545

    add urls for course

--- learning_platform/learning_platform/urls.py
+++ learning_platform/learning_platform/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from core.views import TestAuthView
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/test-auth/", TestAuthView.as_view(), name="test-auth"),
+    path("", include("core.urls")),
 ]
