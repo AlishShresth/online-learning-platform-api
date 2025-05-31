5c7554553c647d85e8312d7c9019c0d66d9a59a2
Alish Shrestha <cresthaalish@gmail.com>
2025-05-31 17:20:27 +0545

    add permission IsStudent

--- learning_platform/core/permissions.py
+++ learning_platform/core/permissions.py
@@ -9,3 +9,8 @@ class IsInstructor(BasePermission):
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "admin"
+
+
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "student"
